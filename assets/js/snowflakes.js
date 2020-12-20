a2882c9e6ad01dad79af7ca0b701ba5f6aca4747
Madhav Varshney <madhavarshney@gmail.com>
2020-12-20 01:33:52 -0800

    Minor design changes

--- assets/js/snowflakes.js
+++ assets/js/snowflakes.js
@@ -124,3 +124,57 @@ pageHeader.addEventListener('mousemove', function(e) {
 });
 
 init();
+
+// Returns a function, that, when invoked, will only be triggered at most once
+// during a given window of time. Normally, the throttled function will run
+// as much as it can, without ever going more than once per `wait` duration;
+// but if you'd like to disable the execution on the leading edge, pass
+// `{leading: false}`. To disable execution on the trailing edge, ditto.
+function throttle(func, wait, options) {
+    var context, args, result;
+    var timeout = null;
+    var previous = 0;
+    if (!options) options = {};
+    var later = function() {
+        previous = options.leading === false ? 0 : Date.now();
+        timeout = null;
+        result = func.apply(context, args);
+        if (!timeout) context = args = null;
+    };
+    return function() {
+        var now = Date.now();
+        if (!previous && options.leading === false) previous = now;
+        var remaining = wait - (now - previous);
+        context = this;
+        args = arguments;
+        if (remaining <= 0 || remaining > wait) {
+            if (timeout) {
+                clearTimeout(timeout);
+                timeout = null;
+            }
+            previous = now;
+            result = func.apply(context, args);
+            if (!timeout) context = args = null;
+        } else if (!timeout && options.trailing !== false) {
+            timeout = setTimeout(later, remaining);
+        }
+        return result;
+    };
+};
+
+const footer = document.querySelector('footer');
+const footerImg = document.querySelector('.footer-img');
+
+footerImg.style.setProperty('--mouse-x', (window.innerWidth / 2) - (footerImg.clientWidth / 2) + 'px');
+
+footer.addEventListener(
+    'mousemove',
+    throttle((e) => {
+        let xPos = e.clientX - (footerImg.clientWidth / 2);
+
+        if (xPos < 0) xPos = 0;
+        if (xPos > window.innerWidth - footerImg.clientWidth) xPos = window.innerWidth - footerImg.clientWidth;
+
+        footerImg.style.setProperty('--mouse-x', xPos + 'px');
+    }, 1000, { leading: true, trailing: true })
+);
